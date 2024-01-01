8eaf4f28b8f0e875ef395471abd9c57d2711679b
Dilumuene Kiaku <chimiefriends@gmail.com>
2024-01-01 16:16:17 +0100

    O que é migrations e como usar migrations com node.js

---
+++ migrations_nodeJs/db/migrations/20240101132921-create-users.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Users',{
+      id:{
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name:{
+        type: Sequelize.STRING
+      },
+      email:{
+        type: Sequelize.STRING
+      },
+      createdAt:{
+        type: Sequelize.STRING
+      },
+      updatedAt:{
+        type: Sequelize.STRING
+      }
+    });
+  },
+
+  async down (queryInterface) {
+    await queryInterface.dropTable('users');
+  }
+};
